cd1869a37327291f4c308eba2363871c5b2ef128
mojianhuiGZ <mjh_haha@126.com>
2018-03-30 11:49:37 +0800

    delete darknet

--- darknet/yolov2/detect.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-DARKNET=/mnt/data1/projects/darknet/darknet
-
-$DARKNET detect cfg/yolo.2.0.cfg yolo.2.0.weights $1
--- darknet/yolov2/prepare.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ ! -f "yolo.2.0.weights" ]; then
-    wget https://pjreddie.com/media/files/yolo.2.0.weights
-fi
-
